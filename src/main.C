d922fb5b95fb5ee3b8ce31eaaae057018ee19dbd
Cody Permann <codypermann@gmail.com>
2014-07-30 09:49:50 -0600

    Small update to stork

--- src/main.C
+++ src/main.C
@@ -19,6 +19,9 @@ int main(int argc, char *argv[])
   // This creates dynamic memory that we're responsible for deleting
   MooseApp * app = AppFactory::createApp("StorkApp", argc, argv);
 
+  app->legacyUoInitializationDefault() = false;
+  app->legacyUoAuxComputationDefault() = false;
+
   // Execute the application
   app->run();
 
