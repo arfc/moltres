5eaccaa2c870b639c6c3339362e6ccb2968265f8
Sun Myung Park <park_cg@live.com>
2019-10-29 13:19:06 -0500

    HeatPrecursorDecay & HeatPrecursorSource kernels

---
+++ include/kernels/HeatPrecursorDecay.h
@@ -0,0 +1,27 @@
+#ifndefHEATPRECURSORDECAY_H
+#define HEATPRECURSORDECAY_H
+
+#include "Kernel.h"
+#include "ScalarTransportBase.h"
+
+// Forward Declarations
+class HeatPrecursorDecay;
+
+template <>
+InputParameters validParams<HeatPrecursorDecay>();
+
+class HeatPrecursorDecay : public Kernel, public ScalarTransportBase
+{
+public:
+  HeatPrecursorDecay(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  unsigned int _heat_group;
+  RealVectorValue _decay_heat_const;
+};
+
+#endif // HEATPRECURSORDECAY_H
