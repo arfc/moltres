034954d8b511b06b8cf2d8787eacf829aa50a165
smpark7 <park_cg@live.com>
2020-02-27 15:07:25 -0600

    prec and temp loop outflow value postprocessor

---
+++ include/postprocessors/SideCoupledIntegralVariablePostprocessor.h
@@ -0,0 +1,27 @@
+#ifndef SIDECOUPLEDINTEGRALVARIABLEPOSTPROCESSOR_H
+#define SIDECOUPLEDINTEGRALVARIABLEPOSTPROCESSOR_H
+
+#include "SideIntegralVariablePostprocessor.h"
+
+// Forward Declarations
+class SideCoupledIntegralVariablePostprocessor;
+
+template <>
+InputParameters validParams<SideIntegralVariablePostprocessor>();
+
+class SideCoupledIntegralVariablePostprocessor : public SideIntegralVariablePostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  SideCoupledIntegralVariablePostprocessor(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpIntegral() override;
+  virtual Real computeIntegral() override;
+
+  const VariableValue & _weight;
+  Real _divisor;
+};
+
+#endif // SIDECOUPLEDINTEGRALVARIABLEPOSTPROCESSOR_H
---
+++ src/postprocessors/SideCoupledIntegralVariablePostprocessor.C
@@ -0,0 +1,39 @@
+#include "SideCoupledIntegralVariablePostprocessor.h"
+
+registerMooseObject("MoltresApp", SideCoupledIntegralVariablePostprocessor);
+
+template <>
+InputParameters
+validParams<SideCoupledIntegralVariablePostprocessor>()
+{
+  InputParameters params = validParams<SideIntegralVariablePostprocessor>();
+  params.addRequiredCoupledVar("weight",
+                               "The name of the variable that this postprocessor is integrated with");
+  params.addParam<Real>("divisor", 1,
+                                "The value of the divisor to normalize by");
+  return params;
+}
+
+SideCoupledIntegralVariablePostprocessor::SideCoupledIntegralVariablePostprocessor(
+    const InputParameters & parameters)
+  : SideIntegralVariablePostprocessor(parameters),
+    _weight(coupledValue("weight")),
+    _divisor(getParam<Real>("divisor"))
+{
+  addMooseVariableDependency(mooseVariable());
+}
+
+Real
+SideCoupledIntegralVariablePostprocessor::computeQpIntegral()
+{
+  return _u[_qp] * _weight[_qp] / _divisor;
+}
+
+Real
+SideCoupledIntegralVariablePostprocessor::computeIntegral()
+{
+  Real sum = 0;
+  for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+    sum += _JxW[_qp] * computeQpIntegral();
+  return sum;
+}
