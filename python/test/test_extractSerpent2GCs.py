4aafe02f409cc8f3c3791e61149112682f7963a8
Sun Myung Park <park_cg@live.com>
2020-07-31 23:42:25 +0800

    Add inline documentation for tests

--- python/test/test_extractSerpent2GCs.py
+++ python/test/test_extractSerpent2GCs.py
@@ -10,16 +10,17 @@ def test_makePropertiesDir():
     """Testing extractSerpent2GCs.py with Serpent 2 XS data, compared
     with the expected JSON file in the gold directory
     """
-    outdir = 'xs'
-    fileBase = 'msfr'
-    mapFile = 'tempMapping.txt'
-    secbranchFile = 'sec.txt'
-    unimapFile = 'unimapFile.txt'
+    outdir = 'xs'                   # Name of directory to hold xs data
+    fileBase = 'msfr'               # Prefix for xs data filenames
+    mapFile = 'tempMapping.txt'     # Map of branches to temperatures
+    secbranchFile = 'sec.txt'       # Secondary branches, if any
+    unimapFile = 'unimapFile.txt'   # Map of materials to universes
 
     temp_map = ['fuel0 900 \n', 'fuel1 1200 \n',
                 'blanket0 900 \n', 'blanket1 1200 \n']
     uni_map = ['fuel 1 \n', 'blanket 3 \n']
 
+    # Write txt files required as input
     file1 = open(mapFile, 'w')
     file1.writelines(temp_map)
     file1.close()
@@ -31,26 +32,33 @@ def test_makePropertiesDir():
     file3.writelines(uni_map)
     file3.close()
 
+    # Rename .coe file to match material names
     shutil.copyfile('MSFR_base.coe', 'fuel.coe')
     shutil.copyfile('MSFR_base.coe', 'blanket.coe')
+
+    # Run makePropertiesDir
     makePropertiesDir(outdir, fileBase, mapFile, secbranchFile,
                       unimapFile)
+
+    # Delete input files
     os.remove(mapFile)
     os.remove(secbranchFile)
     os.remove(unimapFile)
     os.remove('fuel.coe')
     os.remove('blanket.coe')
 
+    # Material and xs names
     materials = ['fuel', 'blanket']
     xs_list = ['BETA_EFF', 'CHID', 'CHIP', 'DIFFCOEF', 'FISS', 'FLX',
                'INVV', 'KAPPA', 'LAMBDA', 'NSF', 'NUBAR', 'REMXS', 'SP0']
 
+    # Check every xs file against the expected files in ./gold/xs/
     for mat in materials:
         for xs in xs_list:
             path = outdir + '/' + fileBase + '_' + mat + '_' + xs + '.txt'
             data = list(open(path, 'r'))
             expected = list(open('gold/' + path, 'r'))
             assert data == expected
-            os.remove(path)
-    os.rmdir(outdir)
+            os.remove(path)     # Delete xs file after check
+    os.rmdir(outdir)        # Delete empty xs directory
 
--- python/test/test_moltres_xs.py
+++ python/test/test_moltres_xs.py
@@ -12,12 +12,22 @@ def test_serpent_xs():
     """Testing moltres_xs.py with Serpent 2 XS data, compared
     with the expected JSON file in the gold directory
     """
+
+    # Run moltres_xs with Serpent 2 data
     read_input('msfr_xs.inp')
-    with open('msfrXS.json') as f:
+
+    # Load the output json xs file
+    with open('msfrXS.json') as f:  
         data = json.load(f)
+
+    # Load gold copy of json xs file
     with open('gold/msfrXS.json') as h:
         expected = json.load(h)
+
+    # Check if they match
     assert data == expected
+
+    # Delete output json xs file after check
     if os.path.exists('msfrXS.json'):
         os.remove('msfrXS.json')
     else:
@@ -28,12 +38,22 @@ def test_scale_xs():
     """Testing moltres_xs.py with SCALE XS data, compared
     with the expected JSON file in the gold directory
     """
+
+    # Run moltres_xs with SCALE data
     read_input('pin_cell_XS.inp')
+
+    # Load the output json xs file
     with open('PinXS.json') as f:
         data = json.load(f)
+
+    # Load gold copy of json xs file
     with open('gold/PinXS.json') as h:
         expected = json.load(h)
+
+    # Check if they match
     assert data == expected
+
+    # Delete output json xs file after check
     if os.path.exists('PinXS.json'):
         os.remove('PinXS.json')
     else:
