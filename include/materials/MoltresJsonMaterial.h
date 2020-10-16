0888968cfebc78d74ef279c0656498c2de7ebb4d
Katy Huff <katyhuff@users.noreply.github.com>
2020-10-16 09:35:47 -0500

    Revert "Update to new MOOSE JSON package"

--- include/materials/MoltresJsonMaterial.h
+++ include/materials/MoltresJsonMaterial.h
@@ -2,7 +2,7 @@
 #define MOLTRESJSONMATERIAL_H_
 
 #include "NuclearMaterial.h"
-#include "nlohmann/json.h"
+#include "json.h"
 
 class MoltresJsonMaterial;
 
@@ -15,7 +15,7 @@ public:
   MoltresJsonMaterial(const InputParameters & parameters);
 
 protected:
-  void Construct(nlohmann::json xs_root, std::vector<std::string> xsec_names);
+  void Construct(moosecontrib::Json::Value xs_root, std::vector<std::string> xsec_names);
   virtual void computeQpProperties();
 
   std::string _material_key;
