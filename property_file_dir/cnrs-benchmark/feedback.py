af4b496c8c1f74de0f5ec9d61c6e5e4035ab17f2
Sun Myung Park <park_cg@live.com>
2021-03-06 00:27:54 +0800

    Add proper documentaion to feedback.py and references to readme

--- property_file_dir/cnrs-benchmark/feedback.py
+++ property_file_dir/cnrs-benchmark/feedback.py
@@ -2,30 +2,52 @@ import numpy as np
 
 
 def extrapolate(xs_name):
-    """ Extrapolates cross section data from 900 K to 1500 K at 50 K intervals
-    based on the formula from Tiberga et al. (2020)
+    """Extrapolate cross section based on thermal salt expansion feedback.
+
+    Extrapolates cross section data at 900 K to 1500 K at 50 K intervals
+    based on the thermal salt expansion feedback formula from [1]. Writes
+    the extrapolated data back into the .txt cross section files in the
+    Moltres-compatible format.
+    
+    Parameters
+    ----------
+    xs_name : list of str
+        Names of cross sections to be extrapolated.
+
+    Returns
+    -------
+    None
+
+    References
+    ----------
+    [1] Tiberga et al., "Results from a multi-physics nurmerical benchmark for
+    codes dedicated to molten salt fast reactors," Annals of Nuclear Energy,
+    vol. 142, July 2020, 107428.
     """
 
     rho_900 = 2.0e3         # Density at 900 K [kg m-3]
-    alpha = 2.0e-4      # Thermal expansion coeff [K-1]
+    alpha = 2.0e-4          # Thermal expansion coeff [K-1]
     input_file = "benchmark_" + xs_name + ".txt"
 
-    # Setup temperature values
+    # Setup temperature values to extrapolate to
     temp = np.linspace(950, 1500, 12)
 
+    # Read cross section data at 900K
     f = open(input_file, 'r+')
     lines = f.readlines()
     data_900 = list(lines[0].split())
     f.close()
 
+    # Setup space separated data to be written back into txt
     s = " "
     xs = [s.join(data_900) + "\n"]
     h = open(input_file, 'w')
 
     for i in range(len(temp)):
-        # Calculate density at 1500 K
+        # Calculate density at temp[i]
         rho = rho_900 * (1 - alpha * (temp[i]-900))
 
+        # Apply extrapolation formula at temp[i]
         data_next = [0, ] * len(data_900)
         data_next[0] = str(temp[i])
         for i in range(1, len(data_900)):
@@ -39,6 +61,7 @@ def extrapolate(xs_name):
         data_next = s.join(data_next) + "\n"
         xs.append(data_next)
 
+    # Write cross section data into txt file
     h = open(input_file, 'w')
     h.writelines(xs)
     h.close()
@@ -46,10 +69,13 @@ def extrapolate(xs_name):
 
 
 def main():
+    """Runs extrapolate() for the relevant cross sections.
+    """
     xs_names = ["DIFFCOEF", "FISS", "NSF", "REMXS", "SP0"]
     for i in xs_names:
         extrapolate(i)
     return
 
 
-main()
+if __name__ == "__main__":
+    main()
