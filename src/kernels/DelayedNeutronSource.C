aaf7585d504db6b3311693cd1539e41b9a2516c7
Sun Myung Park <smpark3@illinois.edu>
2021-04-22 06:27:58 -0500

    Add eigenvalue_scaling parameter to fission and delayed neutron source kernels

--- src/kernels/DelayedNeutronSource.C
+++ src/kernels/DelayedNeutronSource.C
@@ -13,6 +13,7 @@ DelayedNeutronSource::validParams()
                                             "concentrations. These MUST be listed by increasing "
                                             "group number.");
   params.addRequiredParam<unsigned int>("group_number","neutron energy group number for chi_d");
+  params.addParam<Real>("eigenvalue_scaling", 1.0, "Eigenvalue scaling factor.");
   return params;
 }
 
@@ -25,7 +26,8 @@ DelayedNeutronSource::DelayedNeutronSource(const InputParameters & parameters)
     _chi_d(getMaterialProperty<std::vector<Real>>("chi_d")),
     _num_precursor_groups(getParam<unsigned int>("num_precursor_groups")),
     _temp_id(coupled("temperature")),
-    _temp(coupledValue("temperature"))
+    _temp(coupledValue("temperature")),
+    _eigenvalue_scaling(getParam<Real>("eigenvalue_scaling"))
 {
   unsigned int n = coupledComponents("pre_concs");
   if (!(n == _num_precursor_groups))
@@ -47,6 +49,9 @@ DelayedNeutronSource::computeQpResidual()
   Real r = 0;
   for (unsigned int i = 0; i < _num_precursor_groups; ++i)
     r += -_decay_constant[_qp][i] * computeConcentration((*_pre_concs[i]), _qp);
+
+  if (!(_eigenvalue_scaling == 1.0))
+    r /= _eigenvalue_scaling;
 
   return _chi_d[_qp][_group-1] * _test[_i][_qp] * r;
 }
@@ -75,6 +80,9 @@ DelayedNeutronSource::computeQpOffDiagJacobian(unsigned int jvar)
     for (unsigned int i = 0; i < _num_precursor_groups; ++i)
       jac += -_test[_i][_qp] * computeConcentration((*_pre_concs[i]), _qp) *
              _d_decay_constant_d_temp[_qp][i] * _phi[_j][_qp];
+
+  if (!(_eigenvalue_scaling == 1.0))
+    jac /= _eigenvalue_scaling;
 
   return _chi_d[_qp][_group-1] * jac;
 }
