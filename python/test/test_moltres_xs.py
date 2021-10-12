8df8a723873ad7b0d5e9f0228871903b8d4d438a
gwenchee <gwendolynchee95@gmail.com>
2021-10-12 12:00:20 -0500

    openmc test

--- python/test/test_moltres_xs.py
+++ python/test/test_moltres_xs.py
@@ -1,8 +1,4 @@
 import json
-import argparse
-import numpy as np
-from pyne import serpent
-import sys
 import os
 
 
@@ -13,7 +9,6 @@ def test_serpent_xs():
 
     # Run moltres_xs with Serpent 2 data
     os.system('python ../moltres_xs.py msfr_xs.inp')
-
 
     # Load the output json xs file
     with open('msfrXS.json') as f:
@@ -55,5 +50,31 @@ def test_scale_xs():
     # Delete output json xs file after check
     if os.path.exists('PinXS.json'):
         os.remove('PinXS.json')
+    else:
+        print('Attempted to delete JSON output file but it does not exist')
+
+
+def test_openmc_xs():
+    """Testing moltres_xs.py with OpenMC XS data, compared with the expected 
+    JSON file in the gold directory
+    """
+
+    # Run moltres_xs with OpenMC data
+    os.system('python ../moltres_xs.py godiva/godiva_openmc.inp')
+
+    # Load the output json xs file
+    with open('godiva/godiva_openmc.json') as f:
+        data = json.load(f)
+
+    # Load gold copy of json xs file
+    with open('gold/godiva.json') as h:
+        expected = json.load(h)
+
+    # Check if they match
+    assert data == expected
+
+    # Delete output json xs file after check
+    if os.path.exists('godiva/godiva_openmc.json'):
+        os.remove('godiva/godiva_openmc.json')
     else:
         print('Attempted to delete JSON output file but it does not exist')
