15abead74d9e189f82d4464ae20361204892b3de
smpark7 <park_cg@live.com>
2024-01-23 01:07:55 -0600

    Remove cross diffusion model

--- src/materials/CDTauMaterial.C
+++
@@ -1,8 +0,0 @@
-#include "CDTauMaterial.h"
-#include "SATauMaterial.h"
-#include "INSADTauMaterial.h"
-#include "INSADMaterial.h"
-
-registerMooseObject("MoltresApp", CDTauMaterial);
-
-template class CDTauMaterialTempl<SATauMaterial>;
